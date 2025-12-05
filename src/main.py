bbd37fd3a5638b5d6ca5f934b820b442e15ddf50
Me88_88 <68464396+eu888@users.noreply.github.com>
2025-12-05 22:28:19 +0000

    Setting up 14

--- src/main.py
+++ src/main.py
@@ -9,11 +9,10 @@ load_dotenv()
 GUILD_ID = 1446539319140679813
 
 intents = discord.Intents.all()
-intents.members =
+intents.members = True
 bot = commands.Bot(command_prefix=prefix, help_command=None, intents=intents)
 bot.author_id = 729290819151855698
 tree = bot.tree
- True
 intents.message_content = True
 prefix = "/"
 @bot.event
